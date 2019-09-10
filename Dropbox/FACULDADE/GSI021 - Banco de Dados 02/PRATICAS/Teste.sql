9321bcf097df1302be585277bf7808852975fd3f
BrunoOlliver <bruno_olliver14@hotmail.com>
2019-09-09 23:47:56 -0300

    pratica(2018-03-16)

---
+++ Dropbox/FACULDADE/GSI021 - Banco de Dados 02/PRATICAS/Teste.sql
@@ -0,0 +1,9 @@
+SELECT * FROM paciente;
+/*
+CREATE SEQUENCE dobra
+    START WITH 102
+    INCREMENT BY 2;
+    
+INSERT INTO paciente VALUES (DOBRA.NEXTVAL,'Maria das Graças','12/10/1970','Rua Pindaiba n 106');
+*/
+SELECT dobra.NEXTVAL FROM DUAL;
